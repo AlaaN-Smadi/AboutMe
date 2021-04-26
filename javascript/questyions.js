8183f8f0e096bf8c1dcae78ab4f9dd0e5cb8a8d7
AlaaN-Smadi <alaasmadi1010@gmail.com>
2021-04-26 18:30:40 +0300

    add new questions

--- javascript/questyions.js
+++ javascript/questyions.js
@@ -1,2 +1,12 @@
 'use strict'
 
+
+function myFunction() {
+    let question1 = prompt("Are you planing to join our comunity and learn HTML,CSS & JavaScript ?? [y/n]");
+    let question2 = prompt("Do you think it will be nice journey while studying with us ?? [y/n]");
+    let question3 = prompt("Do you think it will be easy journey ?? [y/n]");
+
+
+}
+
+
