a7f86cc8484e217494af9182a0f1772e02b205ef
AlaaN-Smadi <alaasmadi1010@gmail.com>
2021-04-26 18:41:28 +0300

    Add new questions

--- javascript/questyions.js
+++ javascript/questyions.js
@@ -5,7 +5,8 @@ function myFunction() {
     let question1 = prompt("Are you planing to join our comunity and learn HTML,CSS & JavaScript ?? [y/n]");
     let question2 = prompt("Do you think it will be nice journey while studying with us ?? [y/n]");
     let question3 = prompt("Do you think it will be easy journey ?? [y/n]");
-
+    let question4 = prompt("Do you think I will be success development ?? [y/n]");
+    let question5 = prompt("Are you interesting to start ?? [y/n]");
 
 }
 
