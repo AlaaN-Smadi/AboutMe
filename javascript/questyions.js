f94798a58d56bc9a682194170ea50f93c16c3361
AlaaN-Smadi <alaasmadi1010@gmail.com>
2021-04-26 20:31:17 +0300

    Add new styles and show result

--- javascript/questyions.js
+++ javascript/questyions.js
@@ -6,11 +6,6 @@ function myFunction() {
     let grade = 0;
 
     let question1 = prompt("Are you planing to join our comunity and learn HTML,CSS & JavaScript ?? [y/n]");
-    let question2 = prompt("Do you think it will be nice journey while studying with us ?? [y/n]");
-    let question3 = prompt("Do you think it will be easy journey ?? [y/n]");
-    let question4 = prompt("Do you think I will be success development ?? [y/n]");
-    let question5 = prompt("Are you interesting to start ?? [y/n]");
-
     switch (question1.toLowerCase()) {
         case "y":
             console.log("first question true answer");
@@ -23,8 +18,10 @@ function myFunction() {
         default:
             console.log("first question false answer");
             grade = grade;
+            break;
 
     }
+    let question2 = prompt("Do you think it will be nice journey while studying with us ?? [y/n]");
     switch (question2.toLowerCase()) {
         case "y":
             console.log("second question true answer");
@@ -37,8 +34,10 @@ function myFunction() {
         default:
             console.log("second question false answer");
             grade = grade;
+            break;
 
     }
+    let question3 = prompt("Do you think it will be easy journey ?? [y/n]");
     switch (question3.toLowerCase()) {
         case "n":
             console.log("third question true answer");
@@ -51,8 +50,12 @@ function myFunction() {
         default:
             console.log("third question false answer");
             grade = grade;
+            break;
 
     }
+    
+    let question4 = prompt("Do you think I will be success development ?? [y/n]");
+    
     switch (question4.toLowerCase()) {
         case "y":
             console.log("fourth question true answer");
@@ -65,8 +68,10 @@ function myFunction() {
         default:
             console.log("fourth question false answer");
             grade = grade;
+            break;
 
     }
+    let question5 = prompt("Are you interesting to start ?? [y/n]");
     switch (question5.toLowerCase()) {
         case "y":
             console.log("fifth question true answer");
@@ -79,9 +84,17 @@ function myFunction() {
         default:
             console.log("fifth question false answer");
             grade = grade;
+            break;
 
     }
+    
+    
+   
+   
 
+    console.log("the last result " , grade);
+
+    alert("Your result = " + grade );
 
 }
 
