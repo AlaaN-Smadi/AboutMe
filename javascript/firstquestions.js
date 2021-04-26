0c071bf1d7df85814af8a9d8341004080992910f
AlaaN-Smadi <alaasmadi1010@gmail.com>
2021-04-26 16:38:24 +0300

    add new files

---
+++ javascript/firstquestions.js
@@ -0,0 +1,6 @@
+'use strict'
+
+let userName = prompt("Hi bro , Let us know your great name ^_^");
+let age = prompt("Great ! Can you provide us your age ...");
+
+document.writeln(`<h2 style="background: lightblue;font-size: 18px;color : green; "> Welcome ${userName} </h2>`);
